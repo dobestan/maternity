b7adf8c42367c0dfb17375985229bbbc192bab50
dobestan <dobestan@gmail.com>
2015-01-12 00:35:40 +0900

    add feature : get pages from response data

--- maternity/maternity.py
+++ maternity/maternity.py
@@ -1,12 +1,29 @@
 #-*- coding: utf-8 -*-
 
 import requests
+import bs4
 
 
 def get_response(BASE_URL, params):
     response = requests.get(
         BASE_URL,
-        params = params,
+        params=params,
     )
 
     return response
+
+
+BASE_URL = "http://shjw.or.kr/bbs/board.php"
+params = {
+    'bo_table': 'postnataldb',
+    'sca': '서울시'
+}
+response = get_response(BASE_URL, params)
+
+data = bs4.BeautifulSoup(response.text)
+pages = int(
+    data.find(
+        "a",
+        attrs={'class': 'pg_end'}
+    )["href"].split("page=")[-1]
+)
