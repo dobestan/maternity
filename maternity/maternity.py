d52ddc0b43bb737c53e1bcaf45885cb1424dc600
dobestan <dobestan@gmail.com>
2015-01-12 01:23:09 +0900

    add feature : parse page title and contents section

--- maternity/maternity.py
+++ maternity/maternity.py
@@ -4,7 +4,7 @@ import requests
 import bs4
 
 
-def get_response(BASE_URL, params):
+def get_response(BASE_URL, params={}):
     response = requests.get(
         BASE_URL,
         params=params,
@@ -40,6 +40,7 @@ for page in range(1, pages+1):
     )
 """
 
+"""
 response = get_response(
     BASE_URL,
     {
@@ -54,3 +55,23 @@ care_centers = data.findAll("tr")[1::]
 
 for care_center in care_centers:
     detail_page = care_center.findAll("a")[1]["href"]
+"""
+
+response = get_response("http://shjw.or.kr/bbs/board.php?bo_table=postnataldb&wr_id=314&sca=%EC%84%9C%EC%9A%B8%EC%8B%9C&page=1")
+data = bs4.BeautifulSoup(response.text)
+
+information = {}
+
+title = data.find(
+    "h1",
+    attrs={
+        'id': 'bo_v_title'
+    }
+).text.strip()
+
+contents = data.find(
+    "section",
+    attrs={
+        'id': 'bo_v_atc',
+    },
+)
