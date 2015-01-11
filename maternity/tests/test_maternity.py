8a3850ac002aed41797d686e6362cbe0bbd8d9a0
dobestan <dobestan@gmail.com>
2015-01-12 00:10:25 +0900

    add test : valid if requested url is as i expected

--- maternity/tests/test_maternity.py
+++ maternity/tests/test_maternity.py
@@ -4,6 +4,14 @@ import maternity
 
 
 class TestMaternity(unittest.TestCase):
+    def test_target_url_is_valid(self):
+        self.assertEqual(
+            maternity.get_response().url,
+            "http://shjw.or.kr/bbs/board.php" + \
+            "?bo_table=postnataldb" + \
+            "&sca=%EC%84%9C%EC%9A%B8%EC%8B%9C"
+        )
+
     def test_get_response_should_get_response(self):
         self.assertEqual(
             maternity.get_response().status_code,
